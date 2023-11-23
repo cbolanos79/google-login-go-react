f180d86895c7a7a57476d16e0ac224882b7c7ebd
Cristo Bolaños <cbolanos@gmail.com>
2023-11-23 13:30:41 +0000

    WIP: basic code for POST /login/google endpoint in backend

--- app/main.go
+++ app/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-    "github.com/labstack/echo/v4"
-    "github.com/labstack/echo/v4/middleware"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"google.golang.org/api/idtoken"
 )
 
 func main() {
-    e := echo.New()
-    e.Use(middleware.CORS())
+	google_client_id := os.Getenv("GOOGLE_CLIENT_ID")
 
-    // TODO: Validate user token with google
-    e.Logger.Fatal(e.Start(":8080"))
+	e := echo.New()
+	e.Use(middleware.CORS())
+
+	// Validate user token with google
+	e.POST("/login/google", func(c echo.Context) error {
+		// TODO: get credentials from JSON request
+
+		credentials := ""
+		payload, err := idtoken.Validate(context.Background(), credentials, client_id)
+
+		// TODO: check if payload is valid
+		// TODO: send response
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
