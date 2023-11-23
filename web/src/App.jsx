ea0bc9ea4895489637a51b8ba41036f679d0a5df
Cristo Bolaños <cbolanos@gmail.com>
2023-11-23 20:12:04 +0000

    feat: added google login button and wellcome message to main web page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,32 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const clientId = import.meta.env['VITE_GOOGLE_CLIENT_ID']
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Wellcome to the login page</p>
+        <p>Please choose your google account to do login</p>
+        <br /><br />
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+        />
+        </GoogleOAuthProvider>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
