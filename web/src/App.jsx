3481780ebea3f84f16e7834bf3c3731bb0bf2826
Cristo <cbolanos@gmail.com>
2023-11-23 23:49:32 +0000

    feat: added Card component in web

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,6 +5,21 @@ import './App.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { GoogleLogin } from '@react-oauth/google';
 
+function Card({type, content}) {
+  let class_name = "card text-white"
+  if (type == "success") {
+    class_name += " bg-darkgreen"
+  } else if (type == "error") {
+    class_name += " bg-darkred"
+  }
+
+  return (
+    <div className={class_name}>
+      {content}
+    </div>
+  )
+}
+
 function App() {
   const clientId = import.meta.env['VITE_GOOGLE_CLIENT_ID']
 
